f5866b54dd52818155314cc9a9bfaf997c8b7287
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2025-05-31 09:04:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,26 @@
 import os
+
 # Bot credentials
 API_ID = '25193832'
 API_HASH = 'e154b1ccb0195edec0bc91ae7efebc2f'
-# MongoDB connection string
-DB_URI = 'mongodb+srv://copy:copy@cluster0.q6zqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-LOG_CHANNEL = '-1002240372506'
-TOKEN = "7141521775:AAEIpE2pDzaPNGMttlSZk"
-OWNER_ID = 7875192045  # Change to actual owner ID
-ALERT_CHANNEL_ID = "-1002329693689"
-MEDIA_DIR = "../media"
-DB_PATH = "nsfw_bot.db"
-AUTH_USERS = [7875192045]
-DB_NAME = "BroadcastBot"
-BROADCAST_AS_COPY = True
-BOT_OWNER_ID = 7875192045
-OWNER_IDS = [7875192045, 6656608288, 6545754981]  # Add as many as you want!
-IMAGES_DIR = ( "https://files.catbox.moe/f23vlq.jpg")
 
+# MongoDB connection string (primary database)
+MONGODB_URI = 'mongodb+srv://guardian:guardian@cluster0.thn0z3g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+# Telegram bot settings
+TOKEN = "7141521775:AAEIpE2pDzaPNGMttlSZk"
+OWNER_ID = 7875192045  # Change to your actual owner ID
+OWNER_IDS = [7875192045, 6656608288, 6545754981]  # Add as many as you want!
+AUTH_USERS = [7875192045]
+
+# Log and alert channels
+LOG_CHANNEL = '-1002240372506'
+ALERT_CHANNEL_ID = "-1002329693689"
+
+# Other settings
+MEDIA_DIR = "../media"
+IMAGES_DIR = "https://files.catbox.moe/f23vlq.jpg"
+BROADCAST_AS_COPY = True
+
+# For backward compatibility, if any code still looks for DB_URI:
+DB_URI = MONGODB_URI
