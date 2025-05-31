9044e265b368387dba72d2ee5c5c1c77e9d49afb
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2025-05-31 11:17:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,11 +43,11 @@ async def main():
 
     # Only scan media messages (photo, video, sticker, document: image/video)
     media_filter = (
-        filters.PHOTO
-        | filters.VIDEO
-        | filters.STICKER
-        | filters.Document.IMAGE
-        | filters.Document.VIDEO
+    filters.PHOTO
+    | filters.VIDEO
+    | filters.Sticker.ALL
+    | filters.Document.IMAGE
+    | filters.Document.VIDEO
     )
     application.add_handler(MessageHandler(media_filter, handle_media))
 
